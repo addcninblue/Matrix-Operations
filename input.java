15d61f44dcd44efb1bef235674c8194845be9200
addcninblue <addison95132@gmail.com>
2016-09-16 10:58:24 -0700

    added input and rewrote some logic in main

--- input.java
+++ input.java
@@ -1,26 +1,33 @@
 import java.util.Scanner;
 public class input {
-    public int[][] getUserInput(){
+    public int[][] getUserMatrix(){
         Scanner in = new Scanner(System.in);
 	    System.out.print("Please input the number of rows in your matrix. ");
         int size = in.nextInt();
         System.out.print("Please input the " + (size * size) + " numbers inside your matrix, separated by a space. (without solution matrix) ");
         in.nextLine(); // clears the \n character from input stream -_____-
-        int row = 0;
-        int column = 0;
         int[][] userInputMatrix = new int[size][size];
         for (int i = 0; i < size; i++){
             for (int j = 0; j < size; j++){
                 userInputMatrix[i][j] = in.nextInt();
             }
         }
+        return userInputMatrix;
 
         // for (int i = 0; i < size; i++){
         //     for (int j = 0; j < size; j++){
         //         System.out.print(userInputMatrix[i][j]);
         //     }
         // }
-
-        return userInputMatrix;
+    }
+    public int[] getSolutionsMatrix(int size){
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please input the solutions matrix, separated by a space: ");
+        int[] solutionsMatrix = new int[size];
+        for (int i = 0; i < size; i++){
+                solutionsMatrix[i] = in.nextInt();
+        }
+        
+        return solutionsMatrix;
     }
 }
--- main.java
+++ main.java
@@ -2,8 +2,9 @@ public class main {
     public static void main(String[] args) {
         determinant det = new determinant(); //imports the determinant class
         input input = new input(); //class for getting user's input
-        int[][] matrix = input.getUserInput(); //store's user's input as matrix
-        int[][] testSubmatrix = det.Submatrix(matrix, 2, 1); //testing Submatrix function
+        int[][] inputMatrix = input.getUserMatrix(); //store's user's input as matrix
+        int[] solutionsMatrix = input.getSolutionsMatrix(inputMatrix.length);
+        int[][] testSubmatrix = det.Submatrix(inputMatrix, 2, 1); //testing Submatrix function
         for(int i = 0; i< testSubmatrix.length; i++){
             for(int k = 0; k< testSubmatrix[0].length;k++){
                 System.out.print(testSubmatrix[i][k] + " ");
