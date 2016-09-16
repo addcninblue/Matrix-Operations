f8803982091b277cd1551ac41b8085f2141ffec3
addcninblue <addison95132@gmail.com>
2016-09-16 10:13:29 -0700

    added working input

--- input.java
+++ input.java
@@ -4,10 +4,23 @@ public class input {
         Scanner in = new Scanner(System.in);
 	    System.out.print("Please input the number of rows in your matrix. ");
         int size = in.nextInt();
-        System.out.print("Please input the " + Math.pow(size, 2) + " numbers inside your matrix, separated by a space. (without solution matrix) ");
+        System.out.print("Please input the " + (size * size) + " numbers inside your matrix, separated by a space. (without solution matrix) ");
         in.nextLine(); // clears the \n character from input stream -_____-
-        String userInput = in.nextLine(); // gets the user's input in String form
-        int[][] userInputDeterminant = new int[][] {{1,2,3},{1,2,3},{1,2,3}};
-        return userInputDeterminant;
+        int row = 0;
+        int column = 0;
+        int[][] userInputMatrix = new int[size][size];
+        for (int i = 0; i < size; i++){
+            for (int j = 0; j < size; j++){
+                userInputMatrix[i][j] = in.nextInt();
+            }
+        }
+
+        // for (int i = 0; i < size; i++){
+        //     for (int j = 0; j < size; j++){
+        //         System.out.print(userInputMatrix[i][j]);
+        //     }
+        // }
+
+        return userInputMatrix;
     }
 }
