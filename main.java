049a45d9d56ff918e2055fa91749d3554da6a058
addcninblue <addison95132@gmail.com>
2016-09-15 20:21:30 -0700

    changed brackets

--- main.java
+++ main.java
@@ -4,10 +4,8 @@ public class main {
         input input = new input(); //class for getting user's input
         int[][] matrix = input.getUserInput(); //store's user's input as matrix
         int[][] testSubmatrix = det.Submatrix(matrix, 2, 1); //testing Submatrix function
-        for(int i = 0; i< testSubmatrix.length; i++)
-        {
-            for(int k = 0; k< testSubmatrix[0].length;k++)
-            {
+        for(int i = 0; i< testSubmatrix.length; i++){
+            for(int k = 0; k< testSubmatrix[0].length;k++){
                 System.out.print(testSubmatrix[i][k] + " ");
             }
         }
