ae28826143f2c8688274f23f1ddcaa9828a5be5e
Addison Chan <addison95132@gmail.com>
2016-09-16 21:46:27 -0700

    added *very* hacky formatting

--- main.java
+++ main.java
@@ -1,3 +1,5 @@
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
 public class main {
     public static void main(String[] args) {
         determinant det = new determinant(); //imports the determinant class
@@ -7,6 +9,7 @@ public class main {
         double[][] inverseMatrix = det.Inverse(inputMatrix);
         // print out finalMatrix (nicely)
         double[] finalMatrix = Multiplication(inverseMatrix, solutionsMatrix);
+        System.out.print("┌"); //matrix top
         for (int i = 0; i < finalMatrix.length; i++){ //rows
             for (int j = 0; j <= finalMatrix.length; j++){ //columns
                 if(j < finalMatrix.length){ //coefficient column
@@ -16,8 +19,28 @@ public class main {
                         System.out.print("0 ");
                     }
                 } else { // solution column
-                    System.out.println(finalMatrix[i]);
+                    if (finalMatrix[i] < 0){
+                        NumberFormat formatter = new DecimalFormat("#0.0");
+                        System.out.print(formatter.format(finalMatrix[i]));
+                    } else {
+                        NumberFormat formatter = new DecimalFormat("#0.00");
+                        System.out.print(formatter.format(finalMatrix[i]));
+                    }
+                    // System.out.print(finalMatrix[i]);
+                    if(i == 0){
+                        System.out.println("┐");
+                    } else if (i== finalMatrix.length - 1){
+                        System.out.println("┘");
+                    } else {
+                        System.out.println("│");
+                    }
+                    if (i == finalMatrix.length - 2){
+                        System.out.print("└");
+                    }
                 }
+            }
+            if (i < finalMatrix.length - 2){
+                System.out.print("│");
             }
         }
         // end print
