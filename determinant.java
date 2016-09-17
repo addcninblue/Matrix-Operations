39c5c7dc862aa9cbacbad35149a114f790c77bdd
Addison Chan <addison95132@gmail.com>
2016-09-16 17:32:58 -0700

    output implemented

--- determinant.java
+++ determinant.java
@@ -1,12 +1,12 @@
 public class determinant {
-    public int Determinant(int[][] matrix) {
-        int det = 0; //determinant
+    public double Determinant(double[][] matrix) {
+        double det = 0; //determinant
         int side = matrix.length; //side length of matrix
         if(side==1) {
             det = matrix[0][0];
         } else { // side greater than 1
             for(int i = 0; i < side; i++) {
-                int detTerm = matrix[0][i] * Determinant(Submatrix(matrix,0,i));
+                double detTerm = matrix[0][i] * Determinant(Submatrix(matrix,0,i));
                 if(i%2==0) {
                     det += detTerm;
                 }else{
@@ -16,10 +16,10 @@ public class determinant {
         }
         return det;
     }
-    public double[][] Inverse(int[][]matrix)
+    public double[][] Inverse(double[][]matrix)
     {
-        int det = Determinant(matrix);
-        int[][] cofactor = new int[matrix.length][matrix.length];
+        double det = Determinant(matrix);
+        double[][] cofactor = new double[matrix.length][matrix.length];
         for(int i = 0; i < matrix.length; i++)//cofactor matrix
         {
             for(int k = 0; k < matrix.length; k++)
@@ -38,10 +38,10 @@ public class determinant {
         }
         return inverse;
     }
-    public int[][] Submatrix(int[][]matrix, int ignoredRow, int ignoredColumn) 
+    public double[][] Submatrix(double[][]matrix, int ignoredRow, int ignoredColumn) 
     {
         int subLength = matrix.length - 1; //length of submatrix
-        int[][] submatrix = new int[subLength][subLength];
+        double[][] submatrix = new double[subLength][subLength];
         for(int row = 0; row < matrix.length; row++)
         {
             if(row == ignoredRow) continue;
--- main.java
+++ main.java
@@ -2,20 +2,26 @@ public class main {
     public static void main(String[] args) {
         determinant det = new determinant(); //imports the determinant class
         input input = new input(); //class for getting user's input
-        int[][] inputMatrix = input.getUserMatrix(); //store's user's input as matrix
-        int[] solutionsMatrix = input.getSolutionsMatrix(inputMatrix.length);
-        int[][] testSubmatrix = det.Submatrix(inputMatrix, 2, 1); //testing Submatrix function
-        for(int i = 0; i< testSubmatrix.length; i++){
-            for(int k = 0; k< testSubmatrix[0].length;k++){
-                System.out.print(testSubmatrix[i][k] + " ");
+        multiplication multiplication = new multiplication(); //class for getting multiplication of two matrices
+        double[][] inputMatrix = input.getUserMatrix(); //store's user's input as matrix
+        double[] solutionsMatrix = input.getSolutionsMatrix(inputMatrix.length); // solutions matrix
+        double[][] inverseMatrix = det.Inverse(inputMatrix);
+        // TODO: implement multiplication
+        // print out finalMatrix (nicely)
+        double[] finalMatrix = multiplication.multiplication(inputMatrix, solutionsMatrix);
+        for (int i = 0; i < finalMatrix.length; i++){ //rows
+            for (int j = 0; j <= finalMatrix.length; j++){ //columns
+                if(j < finalMatrix.length){ //coefficient column
+                    if(i == j){
+                        System.out.print("1 ");
+                    } else {
+                        System.out.print("0 ");
+                    }
+                } else { // solution column
+                    System.out.println(finalMatrix[i]);
+                }
             }
         }
+        // end print
     }
-    /*static int[][] InverseMatrix(int[][] matrix)
-    {
-        int matrixLength = matrix.length;
-        int[][] inverseMatrix = new int[matrixLength][matrixLength];
-        int matrixDeterminant = Determinant(matrix);
-        //continue
-    }*/
 }
