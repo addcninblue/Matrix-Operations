2d16c64773ecafbff20bd56b2bc9660148fbb23e
Addison Chan <addison95132@gmail.com>
2016-09-20 10:10:52 -0700

    added input while loop for y/n

--- input.java
+++ input.java
@@ -6,21 +6,22 @@ public final class input {
         int size = in.nextInt();
         in.nextLine(); // clears the \n character from input stream -_____-
         double[][] userInputMatrix = new double[size][size];
-        // while (true){ TODO
+        while (true){
             System.out.print("Please input the " + (size * size) + " numbers inside your matrix, separated by a space. (without solution matrix) ");
             for (int row = 0; row < size; row++){
                 for (int column = 0; column < size; column++){
                     userInputMatrix[row][column] = in.nextDouble();
                 }
             }
+            in.nextLine();
             System.out.println("Your matrix is:");
             output.printMatrix(userInputMatrix);
-            // System.out.print("\nIs that right? \n y/n: " ); TODO
-            // String yesNo = in.nextLine();
-            // if (yesNo.equalsIgnoreCase("y")){
-            //     break;
-            // }
-        // }
+            System.out.print("\nIs that right? \n y/n: " );
+            String yesNo = in.nextLine();
+            if (yesNo.equalsIgnoreCase("y")){
+                break;
+            }
+        }
         return userInputMatrix;
 
         // for (int i = 0; i < size; i++){
@@ -31,10 +32,20 @@ public final class input {
     }
     public static double[][] getSolutionsMatrix(int size){
         Scanner in = new Scanner(System.in);
-        System.out.print("Please input the solutions matrix, separated by a space: ");
-        double[][] solutionsMatrix = new double[1][size];
-        for (int i = 0; i < size; i++){
-                solutionsMatrix[0][i] = in.nextDouble();
+        double[][] solutionsMatrix = new double[size][1];
+        while(true){
+            System.out.print("Please input the solutions matrix, separated by a space: ");
+            for (int i = 0; i < size; i++){
+                    solutionsMatrix[i][0] = in.nextDouble();
+            }
+            in.nextLine();
+            System.out.println("Your matrix is:");
+            output.printMatrix(solutionsMatrix);
+            System.out.print("\nIs that right? \n y/n: " );
+            String yesNo = in.nextLine();
+            if (yesNo.equalsIgnoreCase("y")){
+                break;
+            }
         }
         
         return solutionsMatrix;
