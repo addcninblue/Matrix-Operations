1baac1c914d62604becdc475e9aa613b045d683a
Addison Chan <addison95132@gmail.com>
2016-09-19 19:21:47 -0700

    added pretty input, but there is an extra space at the end before the bracket. Also, need to implement while loop for user input still

--- input.java
+++ input.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
-public class input {
-    public double[][] getUserMatrix(){
+public final class input {
+    public static double[][] getUserMatrix(){
         Scanner in = new Scanner(System.in);
-	    System.out.print("Please input the number of rows in your matrix. ");
+	    System.out.print("Please input the number of rows in your matrix: ");
         int size = in.nextInt();
-        System.out.print("Please input the " + (size * size) + " numbers inside your matrix, separated by a space. (without solution matrix) ");
         in.nextLine(); // clears the \n character from input stream -_____-
         double[][] userInputMatrix = new double[size][size];
-        for (int i = 0; i < size; i++){
-            for (int j = 0; j < size; j++){
-                userInputMatrix[i][j] = in.nextDouble();
+        // while (true){ TODO
+            System.out.print("Please input the " + (size * size) + " numbers inside your matrix, separated by a space. (without solution matrix) ");
+            for (int row = 0; row < size; row++){
+                for (int column = 0; column < size; column++){
+                    userInputMatrix[row][column] = in.nextDouble();
+                }
             }
-        }
+            System.out.println("Your matrix is:");
+            output.printMatrix(userInputMatrix);
+            // System.out.print("\nIs that right? \n y/n: " ); TODO
+            // String yesNo = in.nextLine();
+            // if (yesNo.equalsIgnoreCase("y")){
+            //     break;
+            // }
+        // }
         return userInputMatrix;
 
         // for (int i = 0; i < size; i++){
@@ -20,7 +29,7 @@ public class input {
         //     }
         // }
     }
-    public double[] getSolutionsMatrix(int size){
+    public static double[] getSolutionsMatrix(int size){
         Scanner in = new Scanner(System.in);
         System.out.print("Please input the solutions matrix, separated by a space: ");
         double[] solutionsMatrix = new double[size];
