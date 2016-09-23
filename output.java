e5e2aa694c48995df6716beeba07c00ede782386
daniel <daniel.phan36@gmail.com>
2016-09-22 17:16:06 -0700

    forgot double precision in one part

--- output.java
+++ output.java
@@ -53,7 +53,7 @@ public final class output {
                         System.out.print("\u2502"); //matrix middle 
                     }
                 }
-                System.out.print(Math.round(100.0*matrix[row][column]) / 100.0);
+                System.out.print(matrix[row][column]);
                 if (column != columns - 1) {
                     System.out.print(" ");
                 }
